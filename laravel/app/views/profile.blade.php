f7947630c814e1b1a5cc82677deded5c3c8aebad
Lukas Bindreiter <lukas.bindreiter@outlook.com>
2014-09-13 14:30:42 +0200

    Profile Form action

--- laravel/app/views/profile.blade.php
+++ laravel/app/views/profile.blade.php
@@ -7,40 +7,40 @@
             <h3>Eingeloggt als Lukas Bindreiter</h3>
             <hr/>
             <h5>Vokabelgruppen</h5>
-            <form class="ui form segment" method="post">
+            {{ Form::open(array('url' => 'profile', 'method' => 'post', 'class' => 'ui form segment')) }}
                 <div class="inline field">
                     <div class="ui checkbox">
-                        <input id="group1" type="checkbox" />
+                        <input tabindex="1" name="group[]" id="group1" type="checkbox" />
                         <label for="group1">Des san de Gruppn!</label>
                     </div>
                 </div>
                 <div class="inline field">
                     <div class="ui checkbox">
-                        <input id="group2" type="checkbox" />
+                        <input tabindex="2" name="group[]" id="group2" type="checkbox" />
                         <label for="group2">Sog ma bitte</label>
                     </div>
                 </div>
                 <div class="inline field">
                     <div class="ui slider checkbox">
-                        <input id="group3" type="checkbox" />
+                        <input tabindex="3" name="group[]" id="group3" type="checkbox" />
                         <label for="group3">woiche Checkbox am besten ausschaut! :D</label>
                     </div>
                 </div>
                 <div class="inline field">
                     <div class="ui slider checkbox">
-                        <input id="group4" type="checkbox" />
+                        <input tabindex="4" name="group[]" id="group4" type="checkbox" />
                         <label for="group4">Gruppe 1</label>
                     </div>
                 </div>
                 <div class="inline field">
                     <div class="ui toggle checkbox">
-                        <input id="group5" type="checkbox" />
+                        <input tabindex="5" name="group[]" id="group5" type="checkbox" />
                         <label for="group5">Gruppe 2</label>
                     </div>
                 </div>
                 <div class="inline field">
                     <div class="ui toggle checkbox">
-                        <input id="group6" type="checkbox" />
+                        <input tabindex="6" name="group[]" id="group6" type="checkbox" />
                         <label for="group6">Gruppe 3</label>
                     </div>
                 </div>
@@ -49,7 +49,7 @@
 
                 <input type="submit" class="ui blue submit button" value="Speichern" />
 
-            </form>
+            {{ Form::close() }}
         </div>
     </div>
 </div>
