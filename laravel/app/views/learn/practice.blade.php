96f220d9c1617c9059d3dd6a68283dd672f7aea2
Lukas Bindreiter <lukas.bindreiter@outlook.com>
2014-09-13 15:19:21 +0200

    Practice progress bars

--- laravel/app/views/learn/practice.blade.php
+++ laravel/app/views/learn/practice.blade.php
@@ -6,8 +6,50 @@
         <div class="ui segment">
             <h3>Vokabeln üben</h3>
             <hr/>
+            <div class="row">
+                <div class="small-12 medium-2 large-2 column">
+                    Stufe 1: 56/100
+                </div>
+                <div class="small-12 medium-6 large-8 column">
+                    <div class="ui blue progress">
+                        <div class="bar" style="width: 56%;">
+                        </div>
+                    </div>
+                </div>
+                <div class="small-12 medium-4 large-2 column">
+                    <a class="ui green submit button">Üben</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="small-12 medium-2 large-2 column">
+                    Stufe 2: 25/100
+                </div>
+                <div class="small-12 medium-6 large-8 column">
+                    <div class="ui blue progress">
+                        <div class="bar" style="width: 25%;"></div>
+                    </div>
+                </div>
+                <div class="small-12 medium-4 large-2 column">
+                    <a class="ui green submit button">Üben</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="small-12 medium-2 large-2 column">
+                    Stufe 3: 19/100
+                </div>
+                <div class="small-12 medium-6 large-8 column">
+                    <div class="ui blue progress">
+                        <div class="bar" style="width: 19%;"></div>
+                    </div>
+                </div>
+                <div class="small-12 medium-4 large-2 column">
+                    <a class="ui green submit button">Üben</a>
+                </div>
+            </div>
 
-            <a href="{{ URL::to('learn') }}" class="ui green submit button">Fertig</a>
+            <hr/>
+
+            <a href="{{ URL::to('learn') }}" class="ui green submit button">Zurück</a>
         </div>
     </div>
 </div>
