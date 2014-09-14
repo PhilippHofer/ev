a951e1757610e3151f9d93a6d7ed0b471682dc90
philipp_hofer <philipphofer@gmx.net>
2014-09-14 19:54:19 +0200

    geht ned :/

--- app/views/learn/train.blade.php
+++ app/views/learn/train.blade.php
@@ -1,19 +1,38 @@
 @extends('layout')
 
-@section('scripts')
-{{ HTML::script('js/train.js') }}
-@show
-
 @section('content')
+<style>
+
+.ui move right reveal{
+	position:relative;
+}
+.hidden{
+	position: absolut;
+    display: inline-block;
+    width: 200px;
+    height: 50px;
+    background-color: green;
+}
+.eins{
+	position: relative;
+}
+</style>
+
 <div class="row">
     <div class="small-12 small-centered column">
         <div class="ui segment">
             <h3>Vokabeln Trainieren</h3>
             <hr/>
-
+            <div id="eins">
+				<div class="ui move right reveal">
+				    <span class="visible content">test</span>
+	 				<span class="hidden content">hallo</span>
+				</div>
+			</div>
             <a href="{{ URL::to('learn') }}" class="ui teal submit button">Fertig</a>
         </div>
     </div>
 </div>
+
 
 @stop
